d3cc62301981f7a96b7abdecc28b26c05fe9f9e3
Bjorn 'Emil' Oudkerk <54799110+Bjorn-O@users.noreply.github.com>
2020-03-24 19:06:00 +0100

    Unlock working, Monsters WIP

--- js/script.js
+++ js/script.js
@@ -16,24 +16,31 @@ let gargoylesAlive = true;
 let keys = 0;
 
 let locations = [];
-location[2] = "Castle's Heart"; // You win
-location[6] = "Green House"; // Vine Monster + Whip
-location[7] = "Dracula's Chambers"; // Dracula
-location[10] = "Castle's Yard"; // Undead + Key
-location[11] = "Gardens"; // Nothing
-location[12] = "Grand Hall"; // Nothing
-location[13] = "Kitchen"; // Key
-location[15] = "Secret Chambers"; // Morning Star
-location[16] = "Library"; // Hidden locked room (Search then Use Key)
-location[17] = "Hallway"; // Nothing
-location[18] = "Dining Hall"; //Nothing
-location[21] = "Guest's Chambers"; //Key
-location[22] = "Entrance"; //Torch
-location[23] = "Storage Room";//Holy Water
-location[25] = "Tower";// Nothing
-location[26] = "Widow's Walk";// Big Key
+locations[2] = "Castle's Heart"; // You win
+locations[6] = "Green House"; // Vine Monster + Whip
+locations[7] = "Dracula's Chambers"; // Dracula
+locations[10] = "Castle's Yard"; // Undead + Key
+locations[11] = "Gardens"; // Nothing
+locations[12] = "Grand Hall"; // Nothing
+locations[13] = "Kitchen"; // Key
+locations[15] = "Secret Chambers"; // Morning Star
+locations[16] = "Library"; // Hidden locked room (Search then Use Key)
+locations[17] = "Hallway"; // Nothing
+locations[18] = "Dining Hall"; //Nothing
+locations[21] = "Guest's Chambers"; //Key
+locations[22] = "Entrance"; //Torch
+locations[23] = "Storage Room";//Holy Water
+locations[25] = "Tower";// Nothing
+locations[26] = "Widow's Walk";// Gargoyles & Big Key
 
-images = [];
+let monsters = [];
+monsters[6] = "Shambling Mound";
+monsters[7] = "Dracula";
+monsters[10] = "Undead";
+monsters[26] = "Gargoyle";
+
+
+let images = [];
 images[0] = "room0.jpg";
 images[1] = "room1.jpg";
 images[2] = "room2.jpg";
@@ -44,7 +51,7 @@ images[6] = "room6.jpg";
 images[7] = "room7.jpg";
 images[8] = "room8.jpg";
 
-directions = [];
+let directions = [];
 directions[2] = ["SOUTH"];
 directions[6] = ["SOUTH"];
 directions[7] = ["SOUTH"]; // Unlock NORTH when Dracula is defeated
@@ -62,32 +69,37 @@ directions[23] = ["NORTH"];
 directions[25] = ["DOWN", "EAST"];
 directions[26] = ["WEST"];
 
-unlockedDirections = [];
+let unlockedDirections = [];
 unlockedDirections[7] = ["SOUTH", "NORTH"];
 unlockedDirections[11] = ["WEST", "SOUTH", "NORTH"];
 unlockedDirections[12] = ["SOUTH", "NORTH"];
 unlockedDirections[16] = ["NORTH", "EAST", "WEST"];
 unlockedDirections[18] = ["NORTH", "WEST", "SOUTH"];
 
-descriptions = [];
+let descriptions = [];
 descriptions[2] = "Before you an orb, shining a brilliant red. The source of the power that fuels both Dracula and his castle. Naught will stop you and no one to take this right away, you've become victorious in your quest.";
-description[6] = "Creeping vines surround the room, crawling and grasping to the walls and floors. In the center a bulb with a gaping maw, secreting a sweet smelling substance and leering at you with his eyeless body.";
+descriptions[6] = "Creeping vines surround the room, crawling and grasping to the walls and floors. In the center a bulb with a gaping maw, secreting a sweet smelling substance and leering at you with his eyeless body.";
 // Change when the Vine Monster is defeated
-description[7] = "Before you, about 10 feet tall, a man clad in black with a face of paleful white. Ears only sharper than the fangs in his dentures. You know his name as does every living soul in these lands, before you stands Dracula."
+descriptions[7] = "Before you, about 10 feet tall, a man clad in black with a face of paleful white. Ears only sharper than the fangs in his dentures. You know his name as does every living soul in these lands, before you stands Dracula."
 // Change when Dracula is defeated
-description[10] = "Graves line melancholic fields of gloomy grass, a single dirt path crossing through. Moans and gurgles from unspeakable horros sound about as the Undead crawl across your view. The restless remaining ever so.";
+descriptions[10] = "Graves line melancholic fields of gloomy grass, a single dirt path crossing through. Moans and gurgles from unspeakable horros sound about as the Undead crawl across your view. The restless remaining ever so.";
 // Change when the Undead are defeated
-description[11] = "Coming from the library, a full garden with exotic flowers and large plants comes into view. Full of life and beauty. To the north, a small greenhouse with it's window's obstructed and the yards towards the west.";
-description[12] = "A vast singular hall, decorated with life-sized portraits and old armours lining the wall. A beautiful rug leads up to an ornament door with a large key-hole in it.";
-description[13] = "A quite modern kitchen, a suspicious lack of garlic.";
-description[15] = "As the door behind the bookshelf opens up, you're greeted by a magically lit room. In the midst of it a single pedestal with a case on it, containing the morning star.";
+descriptions[11] = "Coming from the library, a full garden with exotic flowers and large plants comes into view. Full of life and beauty. To the north, a small greenhouse with it's window's obstructed and the yards towards the west.";
+descriptions[12] = "A vast singular hall, decorated with life-sized portraits and old armours lining the wall. A beautiful rug leads up to an ornament door with a large key-hole in it.";
+descriptions[13] = "A quite modern kitchen, a suspicious lack of garlic.";
+descriptions[15] = "As the door behind the bookshelf opens up, you're greeted by a magically lit room. In the midst of it a single pedestal with a case on it, containing the morning star.";
 // Change whent the Morning Star has been picked up
-description[16] = "A vast collection of knowledge, most of it you cannot comprehend. Magical lights line the ceeling, illuminating the room.";
+descriptions[16] = "A vast collection of knowledge, most of it you cannot comprehend. Magical lights line the ceeling, illuminating the room.";
 // Change when the Hidden door is found
-description[17] = "A simple hallway with many doors, and even a stairway up.";
-description[18] = "A long table decorated with a red runner, large wooden thrones line the sides and it seats 16. A door leads to the kitchen, yet there appears to be a locked cellar door.";
+descriptions[17] = "A simple hallway with many doors, and even a stairway up.";
+descriptions[18] = "A long table decorated with a red runner, large wooden thrones line the sides and it seats 16. A door leads to the kitchen, yet there appears to be a locked cellar door.";
 // Change when the cellar door is opened.
-description[21] = "A collection of rooms, all with a bed, a dressoir, and a small table and chair. Many of these rooms haven't seen use in decades.";
+descriptions[21] = "A collection of rooms, all with a bed, a dressoir, and a small table and chair. Many of these rooms haven't seen use in decades.";
+descriptions[22] = "A tall wooden door from which you entered accompanied by a hallway to the north and a brass door to the west.";
+descriptions[23] = "A damp dark cellar filled with crates and barrels";
+descriptions[25] = "A long stairway heading upwards leading to a singular passageway to the Widow Walk";
+descriptions[26] = "A walk-way around the tower, high up in the air. Suddenly, a crackling noice reveals a disguised foe. A Gargoyle sitting on the edge takes flight."
+// Change when the Gargoyle is defeated.
 
 alternateDescriptions = []; // Will be used to change descriptions of the previous Array.
 
@@ -100,15 +112,19 @@ myInput.addEventListener('keydown', getInput, false);
 function getInput(evt) {
   if (evt.key == "Enter") {
     let inputArray = myInput.value.split(" ");
+    // inputArray.forEach((element) => {
+    //   this.element = text.toUpperCase();
+    // }); Doesn't work. Not necisarry
+
 
     if (inputArray[0] == "GO") {
       if (directions[currentLocation].indexOf(inputArray[1]) != -1) {
         switch (inputArray[1]) {
           case "NORTH":
-            currentLocation -= 3;
+            currentLocation -= 5;
             break;
           case "SOUTH":
-            currentLocation += 3;
+            currentLocation += 5;
             break;
           case "EAST":
             currentLocation += 1;
@@ -138,11 +154,22 @@ function getInput(evt) {
     }
 
     if (inputArray[0] == "USE"){
-      console.log('Use an object.');
       myInput.value = "";
     }
 
-    if (inputArray[0] != "GO" && inputArray[0] != "pak" && inputArray[0] != "gebruik" ){
+    if (inputArray[0] == "UNLOCK"){
+      if (checkLock(currentLocation) == true) {
+        directions[currentLocation] = unlockedDirections[currentLocation];
+        unlockedDirections[currentLocation] = null;
+        giveLocation();
+        console.log("The door is unlocked!");
+      }
+      else {
+        console.log("The door remains locked.");
+      }
+    }
+
+    if (inputArray[0] != "GO" && inputArray[0] != "USE" && inputArray[0] != "UNLOCK" && inputArray[0] != "" ){
       feedback.innerHTML = "Please use; GO, USE, SEARCH";
       myInput.value = "";
       setTimeout(removeFeedback, 4000);
@@ -150,7 +177,33 @@ function getInput(evt) {
 
   }
 }
+//FUNCTIONS TO CHECK THE VARIABLE THINGS ONE CAN FIND IN THE CASTLE
 
+//To see if there's a locked door.
+function checkLock(a) {
+  if (unlockedDirections[a] == null) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+//To see if there's a monster.
+function checkMonster(a) {
+  if (monster[a] == null) {
+    return false;
+    console.log("No Monsters")
+  }
+  else {
+    return true;
+    console.log("AH! Spoopy monstah!")
+  }
+}
+
+
+
+// SHOWS THE DIRECTIONS, INVENTORY AND PICTURE.
 function giveLocation() {
   divLocation.innerHTML = locations[currentLocation];
   myDescription.innerHTML = descriptions[currentLocation];
